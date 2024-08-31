1b1011fda019c5dc7e139f408ca7bbce0ade626a
Edwin Choate <edwin.choate@pm.me>
2024-08-31 10:53:43 -0400

    adds Character class

---
+++ Src/Character.cs
@@ -0,0 +1,15 @@
+﻿
+namespace TextAdventure
+{
+    public abstract class Character
+    {
+
+        public string Name { get; set; }
+        
+        public Character (string name)
+        {
+            Name = name;
+        }
+
+    }
+}
