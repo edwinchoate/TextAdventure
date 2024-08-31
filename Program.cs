e28d8fed99b9926eb9c9ce84ac13813921af7683
Edwin Choate <edwin.choate@pm.me>
2024-08-31 11:25:21 -0400

    tiny refactor

--- Program.cs
+++ Program.cs
@@ -3,15 +3,15 @@
 
 Console.WriteLine("Hullo, traveler. What's yer name?");
 
-string input = Console.ReadLine();
+string name = Console.ReadLine();
 
-while (input.Equals(String.Empty) || input == null)
+while (name.Equals(String.Empty) || name == null)
 {
     Console.WriteLine("I apologize. I'm hard of hearing in me old age. Could yah say yer name again?");
-    input = Console.ReadLine();
+    name = Console.ReadLine();
 }
 
-Player player = new Player(input);
+Player player = new Player(name);
 
 string a = "aeiouAEIOU".IndexOf(player.Name[0]) == -1 ? "a" : "an";
 Console.WriteLine($"Aye. You look like {a} {player.Name}.");
