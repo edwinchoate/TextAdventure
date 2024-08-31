aaebf232379db981d16502c5fb2cf86edd56f972
Edwin Choate <edwin.choate@pm.me>
2024-08-31 10:37:04 -0400

    asks the player their name

--- Program.cs
+++ Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+
+Console.WriteLine("Hullo, traveler. What's yer name?");
+
+string name = Console.ReadLine();
+
+while (name.Equals(String.Empty) || name == null)
+{
+    Console.WriteLine("I apologize. I'm hard of hearing in me old age. Could yah say yer name again?");
+    name = Console.ReadLine();
+}
+
+name = name[0].ToString().ToUpper() + name.Substring(1);
+
+string a = "aeiouAEIOU".IndexOf(name[0]) == -1 ? "a" : "an";
+Console.WriteLine($"Aye. You look like {a} {name}.");
+
