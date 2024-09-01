dcd3d800ddd9fa66bd68d8d79712c3bd80fedfa1
Edwin Choate <edwin.choate@pm.me>
2024-09-01 12:28:47 -0400

    refactor

--- Src/Character.cs
+++ Src/Character.cs
@@ -5,16 +5,16 @@ namespace TextAdventure
     public abstract class Character
     {
         private string _name;
-
-        public string Name 
-        { 
-            get => TextUtils.Capitalize(_name);
-            set => _name = value; 
-        }
         
         public Character (string name)
         {
             Name = name;
+        }
+
+        public string Name
+        {
+            get => TextUtils.Capitalize(_name);
+            set => _name = value;
         }
 
     }
