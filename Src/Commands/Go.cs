0d064c33449ca50ee524a338abadd63d4877e8f4
Edwin Choate <edwin.choate@pm.me>
2024-09-11 20:49:08 -0400

    creates the Go command class

---
+++ Src/Commands/Go.cs
@@ -0,0 +1,39 @@
+﻿
+namespace TextAdventure
+{
+    public class Go : Command
+    {
+        private readonly Map _map;
+
+        public Go (Map map)
+        {
+            _map = map;
+        }
+
+        public override string Name { get => Text.StringResources.GoCommandName; }
+
+        public override void Execute(string[] args)
+        {
+
+            if (_map.CurrentCell != null)
+            {
+                string directionArg = args[1].Substring(0, 1).ToUpper() + args[1].Substring(1).ToLower();
+
+                Enum.TryParse(directionArg, out Directions direction); // a failed parse will automatically set direction to None
+
+                int cellIndex = _map.CurrentCell.Neighbors[direction];
+
+                if (cellIndex == -1 || direction == Directions.None)
+                    Console.WriteLine(Text.StringResources.GoCommandError);
+                else
+                    _map.GoToCell(cellIndex);
+            }
+            else
+            {
+                throw new NullReferenceException();
+            }
+
+        }
+
+    }
+}
