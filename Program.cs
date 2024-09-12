fdb44de39fcd01027583d36568dcdc59c9c99593
Edwin Choate <edwin.choate@pm.me>
2024-09-11 20:49:21 -0400

    starts the main game loop

--- Program.cs
+++ Program.cs
@@ -1,5 +1,7 @@
 ﻿using TextAdventure;
 
+bool gameRunning = true;
+
 Text.LoadStringResources(new EnglishStringResources());
 
 Console.WriteLine(Text.StringResources.WhatsYourName);
@@ -14,6 +16,38 @@ while (name == null || name.Equals(String.Empty))
 
 Player player = new Player(name);
 Map map = new Map(player);
+Invoker.Instance.Register(new Go(map));
 
 Console.WriteLine(Text.StringResources.AffirmName, Text.AOrAn(player.Name), player.Name);
 
+Cell? lastCell = null;
+
+while(gameRunning)
+{
+    if (map.CurrentCell != null)
+    {
+        if (lastCell != map.CurrentCell)
+        {
+            Console.WriteLine(map.CurrentCell.ToString());
+            lastCell = map.CurrentCell;
+        }
+    }
+    else
+    {
+        throw new NullReferenceException();
+    }
+
+    string input = Console.ReadLine() ?? "";
+    input = input.ToLower();
+
+    if (input.Equals(Text.StringResources.Quit, StringComparison.OrdinalIgnoreCase))
+    {
+        gameRunning = false;
+        Console.WriteLine(Text.StringResources.Farewell);
+    }
+    else
+    {
+        Invoker.Instance.Invoke(input.Split(" "));
+    }
+
+}
