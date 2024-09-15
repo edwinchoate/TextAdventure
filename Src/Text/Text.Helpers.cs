64e97927f26b20ac03961039f3fa54e5da945eb6
Edwin Choate <edwin.choate@pm.me>
2024-09-15 10:53:10 -0400

    moves the text helper functions into their own partial file, Text.Helpers

---
+++ Src/Text/Text.Helpers.cs
@@ -0,0 +1,27 @@
+﻿
+namespace TextAdventure
+{
+    public static partial class Text
+    {
+
+        public static string Capitalize(string s)
+        {
+            return s[0].ToString().ToUpper() + s.Substring(1);
+        }
+
+        public static string AOrAn(string nextWord)
+        {
+            return "aeiouAEIOU".IndexOf(nextWord[0]) == -1 ? "a" : "an";
+        }
+
+        public static string CommaSeparatedList(string[] words)
+        {
+            return String.Concat(
+                String.Join(", ", words, 0, words.Length - 1),
+                $" {Text.StringResources.And} ",
+                words[words.Length - 1]
+            );
+        }
+
+    }
+}
--- Src/Text/Text.cs
+++ Src/Text/Text.cs
@@ -1,7 +1,7 @@
 ﻿
 namespace TextAdventure
 {
-    public static class Text
+    public static partial class Text
     {
 
         private static StringResources? _stringResources;
@@ -22,25 +22,5 @@ namespace TextAdventure
             _stringResources = stringResources;
         }
 
-        public static string Capitalize (string s)
-        {
-            return s[0].ToString().ToUpper() + s.Substring(1);
-        }
-
-        public static string AOrAn(string nextWord)
-        {
-            return "aeiouAEIOU".IndexOf(nextWord[0]) == -1 ? "a" : "an";
-        }
-
-        public static string CommaSeparatedList(string[] words)
-        {
-            return String.Concat(
-                String.Join(", ", words, 0, words.Length - 1),
-                $" {Text.StringResources.And} ",
-                words[words.Length - 1]
-            );
-        }
-
     }
-
 }
