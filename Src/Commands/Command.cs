f35db018c31ccac90be4529161a13e0f09a3e3fb
Edwin Choate <edwin.choate@pm.me>
2024-09-16 17:11:53 -0400

    edits Command.cs

--- Src/Commands/Command.cs
+++ Src/Commands/Command.cs
@@ -4,13 +4,11 @@ namespace TextAdventure
     public abstract class Command
     {
 
-        public virtual string Name { get => ""; }
+        public virtual string Name => "";
 
         public virtual void Execute(string[] args)
         {
-            throw new Exception(
-                "Execute in the Command parent class was called. This method is supposed to be overridden."
-            );
+            throw new NotImplementedException();
         }
 
     }
