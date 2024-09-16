6e4106dd694149e9ba172e41dae1e8c09dc90a05
Edwin Choate <edwin.choate@pm.me>
2024-09-16 18:09:30 -0400

    removes unnecessary null checks

--- Src/Map/Map.Move.cs
+++ Src/Map/Map.Move.cs
@@ -9,23 +9,15 @@ namespace TextAdventure
 
         public void GoToCell (int index)
         {
-            if (Cells == null)
-                throw new NullReferenceException();
-            else
-            {
-                if (CurrentCell != null)
-                    CurrentCell.Visited = true;
+            if (CurrentCell != null)
+                CurrentCell.Visited = true;
 
-                CurrentCell = Cells[index];
-            }
+            CurrentCell = Cells[index];
         }
 
         public void GoToStartingCell ()
         {
-            if (Cells == null)
-                throw new NullReferenceException();
-            else 
-                CurrentCell = Cells[0]; // TODO Set starting cell based on the actual content I come up with
+            CurrentCell = Cells[0]; // TODO Set starting cell based on the actual content I come up with
         }
 
     }
