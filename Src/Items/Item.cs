d85b215dbec3d7f535ee95072703fae14a8eef5d
Edwin Choate <edwin.choate@pm.me>
2024-09-16 17:13:36 -0400

    adds Item class

---
+++ Src/Items/Item.cs
@@ -0,0 +1,19 @@
+﻿
+namespace TextAdventure
+{
+    public abstract class Item
+    {
+
+        public virtual string Name { get; set; } = "";
+
+        public bool CanReuse { get; set; }
+
+        public bool CanTake { get; set; } = true;
+
+        public virtual void Use (string user)
+        {
+            throw new NotImplementedException();
+        }
+
+    }
+}
