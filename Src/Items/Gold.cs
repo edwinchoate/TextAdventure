624583000c1646cea68fdd334f77e38c22a86341
Edwin Choate <edwin.choate@pm.me>
2024-09-22 10:52:36 -0400

    adds Gold class

---
+++ Src/Items/Gold.cs
@@ -0,0 +1,27 @@
+﻿
+namespace TextAdventure
+{
+    public class Gold : Item
+    {
+
+        public Gold (int value)
+        {
+            Value = value;
+
+            Takeable = true;
+        }
+
+        public override string Name
+        {
+            get
+            {
+                return String.Format(
+                    Text.StringResources.Gold, Value.ToString(), Text.SingularOrPlural(Text.StringResources.Coin, Value)
+                );
+            }
+        }
+
+        public int Value { get; private set; }
+
+    }
+}
